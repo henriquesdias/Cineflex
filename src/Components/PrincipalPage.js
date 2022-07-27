1975f7f1a731ac77348f1bf785edde736a40004d
henriquesdias <henrique.dias1915@gmail.com>
2022-07-27 17:27:41 -0300

    the films are shown on the principal page

--- src/Components/PrincipalPage.js
+++ src/Components/PrincipalPage.js
@@ -1,6 +1,16 @@
-import PostFilm from "./PostFilme";
+import PostFilm from "./PostFilm";
+import { useState , useEffect } from "react";
+import axios from "axios";
 export default function PrincipalPage(){
-  return (
-    <p>ahuhahau</p>
-  );
+  const [listOfFilms , setListOfFilmes] = useState([]);
+  useEffect( () => {
+    const promise = axios.get(
+      "https://mock-api.driven.com.br/api/v5/cineflex/movies"
+    );
+    promise.then( answer => setListOfFilmes(answer.data));
+  } , []);
+
+  return listOfFilms.map( (film,index) => (
+      <PostFilm URL={film.posterURL} alt={film.title} key={index}/>
+    ));
 }
