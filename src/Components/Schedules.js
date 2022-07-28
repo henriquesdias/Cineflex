41de302c2d4e6456a7d0b1cf1b29efeda52de161
henriquesdias <henrique.dias1915@gmail.com>
2022-07-28 00:52:39 -0300

    when the user clicks on a movie, he is redirected to the route 'filme/:idFilme'

--- src/Components/Schedules.js
+++ src/Components/Schedules.js
@@ -1,19 +1,18 @@
 import { useParams } from "react-router-dom";
+import { useEffect,useState } from "react";
+import axios from "axios";
 
 
 export default function Schedules(){
-  const params = useParams();
-  console.log(params);
+  const idFilm = useParams();
+  const [listOfSessionTime , setListOfSessionTime ] = useState([]);
+  useEffect( () => {
+    const promise = axios.get(`https://mock-api.driven.com.br/api/v5/cineflex/movies/${idFilm}/showtimes`);
+
+  })
   return (
       <>
         <h1 className="title">Selecione o horário</h1>
-        <div className="info">
-          <span>Quinta-feira - 24/06/2021</span>
-          <div>
-            <div className="time">15:00</div>
-            <div className="time">19:00</div>
-          </div>
-        </div>
         <div className="info">
           <span>Quinta-feira - 24/06/2021</span>
           <div>
